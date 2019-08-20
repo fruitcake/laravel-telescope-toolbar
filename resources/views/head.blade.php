571d99bd1075effa70e9a8b492c695a4de84c90a
Barry vd. Heuvel <barry@fruitcake.nl>
2019-08-20 14:25:17 +0200

    Add head

---
+++ resources/views/head.blade.php
@@ -0,0 +1,5 @@
+
+<!-- Start of Telescope Toolbar assets !-->
+<script src="{{ route('telescope-toolbar.baseJs') }}?{{ $assetVersion }}"></script>
+<link href="{{ route('telescope-toolbar.styling') }}?{{ $assetVersion }}" rel="stylesheet">
+<!-- End of Telescope Toolbar assets !-->
