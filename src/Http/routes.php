6dfdbc19e63b1ef10c3476af8d17bbced1c05f64
Koen Hoeijmakers <koen@hoeijmakers.me>
2019-09-13 16:49:55 +0200

    import route facade

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/render/{token}', 'ToolbarController@render')->name('telescope-toolbar.render');
 Route::get('/show/{token}/{tab?}', 'ToolbarController@show')->name('telescope-toolbar.show');
 Route::get('/assets/base.js', 'ToolbarController@baseJs')->name('telescope-toolbar.baseJs');
