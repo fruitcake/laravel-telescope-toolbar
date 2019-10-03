d6c63152ebb65e6541ed506b5f4cbe0fc24de508
Joeri Aben <joeri.aben@gmail.com>
2019-10-03 13:52:32 +0200

    Adding back route facade import

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,4 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('render/{token}', 'ToolbarController@render')->name('telescope-toolbar.render');
 Route::get('show/{token}/{tab?}', 'ToolbarController@show')->name('telescope-toolbar.show');
