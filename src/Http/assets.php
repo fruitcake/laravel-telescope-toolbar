46e8a6d561101fa0291d615dc26fc173949e68f7
Joeri Aben <joeri.aben@gmail.com>
2019-10-03 14:00:43 +0200

    Added route facade import for assets.php

--- src/Http/assets.php
+++ src/Http/assets.php
@@ -1,4 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('base.js', 'ToolbarController@baseJs')->name('telescope-toolbar.baseJs');
 Route::get('styling.css', 'ToolbarController@styling')->name('telescope-toolbar.styling');
