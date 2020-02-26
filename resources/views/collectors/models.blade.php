b0f2b83fe2f36e003bd37a3d57137ca3a38e1614
Victor Lap <victor@elnino.tech>
2020-02-26 21:52:22 +0100

    Show details about models

--- resources/views/collectors/models.blade.php
+++ resources/views/collectors/models.blade.php
@@ -14,5 +14,37 @@
 
     @endslot
 
+    @slot('text')
+
+        <div class="sf-toolbar-info-piece">
+            <table class="sf-toolbar-previews">
+
+                <thead>
+                <tr>
+                    <th>Action</th>
+                    <th>Model</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                @foreach ($entries as $entry)
+                    <tr>
+                        <td>
+                            {{ $entry->content['action'] }}
+                        </td>
+
+                        <td>
+                            <a href="{{ route('telescope') }}/models/{{ $entry->id }}" target="_telescope">
+                                {{ $entry->content['model'] }}
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+
+            </table>
+        </div>
+
+    @endslot
 
 @endcomponent
