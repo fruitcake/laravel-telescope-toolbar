448116a2d7b6df740870ffcc3079bdc87f2e9490
Barry vd. Heuvel <barry@fruitcake.nl>
2023-12-13 11:09:34 +0100

    Tweak widget

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,5 +1,5 @@
 <!-- Start of Telescope Toolbar widget !-->
-<div id="sfwdt{{ $token }}" class="sf-toolbar sf-display-none"></div>
+<div id="sfwdt{{ $token }}" class="sf-toolbar sf-display-none" role="region" aria-label="Laravel Telescope Toolbar"></div>
 
 <script @if(isset($csp_script_nonce) && $csp_script_nonce) nonce="{{ $csp_script_nonce }}" @endif>/*<![CDATA[*/
   (function () {
